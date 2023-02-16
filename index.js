7e8285cb58b77de894241df26fa36ba926fac87d
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-02-16 07:42:36 +0100

    dont use external packages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
     console.log(file);
   });
 
-  const core = require('@actions/core');
+  /* const core = require('@actions/core');
   const github = require('@actions/github');
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -14,7 +14,8 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event payload: ${payload}`); */
 } catch (error) {
-  core.setFailed(error.message);
+  console.log(error.message);
+  /* core.setFailed(error.message); */
 }
