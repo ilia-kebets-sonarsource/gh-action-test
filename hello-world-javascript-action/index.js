a38b9f6df725d8a9837740c47973c2081a5546d0
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-02-16 07:01:09 +0100

    print current folder

--- hello-world-javascript-action/index.js
+++ hello-world-javascript-action/index.js
@@ -1,7 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
+  fs.readdirSync(__dirname).forEach(file => {
+    console.log(file);
+  });
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
